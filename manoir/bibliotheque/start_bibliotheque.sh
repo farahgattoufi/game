54951cf849c5ad1d135b48a6649d6dca0b700e29
farahgattoufi <farah.gattoufi2@gmail.com>
2025-11-05 18:35:58 +0100

    Update start_bibliotheque.sh

--- manoir/bibliotheque/start_bibliotheque.sh
+++ manoir/bibliotheque/start_bibliotheque.sh
@@ -8,10 +8,8 @@ echo
 echo "Une fois trouvé :"
 echo "- Débloque ses permissions"
 echo "- Décompresse-le dans ton coffre pour révéler son message "
-echo "- Décrypte le texte chiffré "
-echo "attention, certains livres t’induiront en erreur !"
-echo "- Modifie le texte après déchiffrage dans ton coffre"  
+echo "- Déchiffre son texte chiffré "
+echo "- Modifie le texte après déchiffrement dans ton coffre"
+echo
 echo "Pour déchiffrer le texte souviens-toi que parfois, il suffit de regarder les choses dans le sens opposé pour voir apparaître la vérité."
-
-echo " Pense a installer bsdmainutils"
 echo "Bonne chasse aux savoirs !"
